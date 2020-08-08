cbf444a55bf4942ed720a74b8549cd7af2ce1dbd
zolter123 <47486680+zolter123@users.noreply.github.com>
2020-08-08 14:31:22 +0300

    08.08 sprint 2 update

---
+++ NatbagFlightLog/src/flight/CompareByDayOfWeek.java
@@ -0,0 +1,17 @@
+package flight;
+
+import java.util.Comparator;
+
+public class CompareByDayOfWeek implements Comparator<Flight>{
+
+
+	@Override
+	public int compare(Flight o1, Flight o2) {
+		if(o1.getDepartureDate().getDayOfWeek().ordinal() > o2.getDepartureDate().getDayOfWeek().ordinal())
+			return 1;
+		if(o1.getDepartureDate().getDayOfWeek().ordinal() < o2.getDepartureDate().getDayOfWeek().ordinal())
+			return -1;
+		return 0;
+	}
+
+}
