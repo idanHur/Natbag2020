3526c441d5ed31f0780c28c6d4388bb6059a70da
idanHur <62388861+idanHur@users.noreply.github.com>
2020-08-30 12:29:52 +0300

    Add files via upload
    python server and natbag project modified to work with html and args[]

---
+++ natbag2020_web_server_flask (1).py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import subprocess
+
+from flask import Flask, request
+
+app = Flask("natbag2020_app")
+
+@app.route("/departures")
+def dep():
+    return subprocess.check_output(["java", "-classpath",
+                                    "C:\\Users\idan1\eclipse-workspace\\NatbagFlightLog.zip_expanded\\NatbagFlightLog\bin", "flight.main",
+                                    request.args.get('outformat'), "departures",
+                                    request.args.get('airline'), request.args.get('country'),
+                                    request.args.get('city'), request.args.get('airport'),
+                                    request.args.get('day1'), request.args.get('month1'),
+                                    request.args.get('year1'), request.args.get('day2'),
+                                    request.args.get('month2'), request.args.get('year2'),
+                                    request.args.get('sunday'), request.args.get('monday'),
+                                    request.args.get('tuesday'), request.args.get('wednesday'),
+                                    request.args.get('thursday'), request.args.get('friday'),
+                                    request.args.get('saturday')])
+
+@app.route("/arrivals")
+def arr():
+    return subprocess.check_output(["java", "-classpath",
+                                    "C:\\Users\idan1\eclipse-workspace\\NatbagFlightLog.zip_expanded\\NatbagFlightLog\bin", "flight.main",
+                                    request.args.get('outformat'), "arrivals",
+                                    request.args.get('airline'), request.args.get('country'),
+                                    request.args.get('city'), request.args.get('airport'),
+                                    request.args.get('day1'), request.args.get('month1'),
+                                    request.args.get('year1'), request.args.get('day2'),
+                                    request.args.get('month2'), request.args.get('year2'),
+                                    request.args.get('sunday'), request.args.get('monday'),
+                                    request.args.get('tuesday'), request.args.get('wednesday'),
+                                    request.args.get('thursday'), request.args.get('friday'),
+                                    request.args.get('saturday')])
+@app.route("/test")
+def test():
+    return subprocess.check_output(["java", "-classpath",
+                                    "C:\\Users\idan1\eclipse-workspace\test\bin", "test.Main"])
+
+app.run(port=8000, host="0.0.0.0")
